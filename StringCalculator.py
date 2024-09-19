59a197fe8fb197d047da28d52607dfdd3bb7ca11
AVINASH SHIVANAND KUMBAR <84611916+Avinashsk2000@users.noreply.github.com>
2024-09-19 17:03:10 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,11 +1,3 @@
-static int add(const char* input) {
-    // If input is NULL or an empty string, return 0
-    if (input == NULL || *input == '\0') {
-        return 0;
-    }
-    // Existing logic for non-empty input
-    char delimiters[3];  
-    char negatives[256] = {0};  
-    const char* numbersPart = extractCustomDelimiter(input, delimiters);
-    return sumNumbers(numbersPart, delimiters, negatives);
-}
+def add(input_data):
+    if not input_data:
+        return 0
